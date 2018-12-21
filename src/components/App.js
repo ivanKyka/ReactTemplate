f1d6bd5052f7733fed5cd81df712afdfd3afadb7
kicha <ivankika.1999@gmail.com>
2018-12-21 10:25:24 +0200

    added mobx

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from "react";
+import {observable} from 'mobx'
 
 import '../styles/App.css';
 
 class App extends Component {
     render() {
+        @observable
+        const a = '123';
         return (
             <div>
             <h1>My React App! </h1>
