02a40ebcaaa5cfea475b53338445a9c5608e21e1
kicha <35963186+HaKicha@users.noreply.github.com>
2019-01-26 16:36:58 +0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
+const webpack = require("webpack");
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     entry: "./src/index.js",
     output: {
+        publicPath: '/',
         path: path.join(__dirname, "/dist"),
         filename: "index-bundle.js"
     },
@@ -13,16 +15,23 @@ module.exports = {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 use: ["babel-loader"]
-            },
-            {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"]
             }
         ]
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: "./src/index.html"
-        })
-    ]
+            template: './src/index.html'
+        }),
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    devtool: 'source-map',
+    devServer: {
+        hot: true,
+        inline: true,
+        contentBase: './',
+        historyApiFallback: true,
+        watchOptions: {
+            ignored: /\/node_modules\/.*/
+        },
+    }
 };
